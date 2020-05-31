105e1b58e3ef6aee558e3446f1a27d0704143a04
andy shao <andy.shaoych@gmail.com>
2020-06-01 00:42:30 +1000

    Initial GitLab

--- packer-demo/vars.tf
+++ packer-demo/vars.tf
@@ -1,9 +1,11 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  # default = "eu-west-1"
+  default = "ap-southeast-2"
 }
 variable "PATH_TO_PRIVATE_KEY" {
   default = "mykey"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  # default = "mykey.pub"
+  default = "/Users/andyshao/.ssh/id_rsa.pub"
 }
